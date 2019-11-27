7af0803a27f24b1b2f43ae328f6d522f972faccd
KODAK <linhtt.it@gmail.com>
2019-11-27 23:13:51 +0700

    2 - add example classes

--- app/src/main/java/com/example/dagger2example/MainActivity.java
+++ app/src/main/java/com/example/dagger2example/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Car car;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        car.drive();
     }
 }
