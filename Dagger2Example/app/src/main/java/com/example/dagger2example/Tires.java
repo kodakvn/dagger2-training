09e5ef9d8e5891e947dab5f589d369f91520dcca
KODAK <linhtt.it@gmail.com>
2019-11-29 22:08:15 +0700

    5 - MODULES & PROVIDER METHODS

---
+++ Dagger2Example/app/src/main/java/com/example/dagger2example/Tires.java
@@ -0,0 +1,11 @@
+package com.example.dagger2example;
+
+import android.util.Log;
+
+public class Tires {
+    private static final String TAG = "kkk";
+
+    public void inflate() {
+        Log.d(TAG, "Tires inflated");
+    }
+}
